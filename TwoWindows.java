ab1f6500d9f0cfe4872efb39ce390e343bd2615f
orici <m_alc1@hotmail.com>
2018-04-14 01:48:10 +0200

    14.04.2018 - 0.0.01     Added personal filenames to exclude to .gitignore     Initialized project - Main files was created

---
+++ TwoWindows.java
@@ -0,0 +1,43 @@
+/**
+ * @file:    TwoWindows.java
+ * @package: javafx
+ *
+ * @author:  Moisés Alcocer, 2018
+ * @link:    https://www.ironwoods.es
+ * @license: http://opensource.org/licenses/MIT - MIT License
+ *
+ * @info: test: building two windows with JavaFx & FXML
+ */
+package javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author orici
+ */
+public class TwoWindows extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass()
+            .getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
